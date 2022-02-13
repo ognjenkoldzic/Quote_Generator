001d85926fc912f7ced75ee6afc391dee16cc0e8
Ognjen Koldzic <ognjen_koldzic@yahoo.de>
2022-02-13 20:44:33 +0100

    minorCahnges

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,7 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
-  const [btnClicked, setBtnClicked] = useState(false);
   const [advice, setAdvice] = useState("");
-  // axios.defaults.headers = {
-  //   "Cache-Control": "no-cache",
-  //   Pragma: "no-cache",
-  //   Expires: "0",
-  // };
 
   // useEffect(() => {
   //   const fetchData = async () => {
@@ -37,17 +31,6 @@ function App() {
     getAdvices();
   }, []);
   console.log(advice);
-
-  // function handleClick() {
-  //   // const fetchData = async () => {
-  //   //   const data = await fetch("https://api.adviceslip.com/advice");
-  //   //   const json = await data.json();
-  //   //   const adviceString = json.slip.advice;
-  //   //   setAdvice(adviceString);
-  //   //   //console.log(adviceString);
-  //   //   fetchData().catch(console.error);
-  //   };
-  //onClick={handleClick}
 
   return (
     <div className="App">
